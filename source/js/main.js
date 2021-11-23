089425e25d2bbb271c4d3bd9cc7d35c5630962ed
Tannnkely <tannnkely@gmail.com>
2021-11-23 21:59:08 +0300

    удален дубликат кода валидации для формы заявки

--- source/js/main.js
+++ source/js/main.js
@@ -67,18 +67,3 @@
     }
   }
 })();
-
-//Validation
-
-(() => {
-  const formElement = document.querySelector('.form-feedback');
-  const Inputmask = window.Inputmask;
-
-  if (formElement) {
-    const telInputElement = formElement.querySelector('#tel');
-    const mask = new Inputmask('9 (999) 999-99-99');
-    if (telInputElement) {
-      mask.mask(telInputElement);
-    }
-  }
-})();
