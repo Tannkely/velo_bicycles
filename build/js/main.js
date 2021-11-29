7202513d0bb0adbd8249c10b75216801c6ea37f6
Tannnkely <tannnkely@gmail.com>
2021-11-29 21:57:01 +0300

    Bug fixed

---
+++ build/js/main.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// Модуль валидации поля формы
+(function () {
+  const formElement = document.querySelector('.form-feedback');
+  if (!formElement) return;
+
+  const telInputElement = formElement.querySelector('#tel');
+  if (!telInputElement) return;
+
+  const mask = new Inputmask('9 (999) 999-99-99');
+  mask.mask(telInputElement);
+})();
+
+'use strict';
+
+// Модуль главного меню (бургер-меню)
+(function () {
+  const mainNavElement = document.querySelector('.main-nav');
+  if (!mainNavElement) return;
+
+  const mainElement = document.querySelector('.main');
+  const mainNavLogoElement = mainNavElement.querySelector('.main-nav__logo');
+  const mainNavToggleElement = mainNavElement.querySelector('.main-nav__toggle');
+  const mainNavSiteNavElement = mainNavElement.querySelector('.main-nav__site-nav');
+  const navItemsCollection = mainNavElement.querySelectorAll('.site-nav__item');
+
+  const elements = {
+    'main-nav': mainNavElement,
+    'main-nav__logo': mainNavLogoElement,
+    'main-nav__toggle': mainNavToggleElement,
+    'main-nav__site-nav': mainNavSiteNavElement,
+  };
+
+  initMenu();
+  mainNavToggleElement.addEventListener('click', toggleMenu);
+
+
+  // Функция иициализации меню
+  function initMenu() {
+
+    // Для каждого элемента описанного в объекте (в карте)
+    Object.keys(elements).forEach((element) => {
+      // если он существует
+      if (elements[element] !== undefined) {
+        // убрать класс --no-js
+        elements[element].classList.remove(`${element}--no-js`);
+        // добавить класс --closed
+        elements[element].classList.add(`${element}--closed`);
+      }
+    });
+
+    // Для каждого элемента пункта меню
+    navItemsCollection.forEach((item) => {
+      // найти ссылку в элементе меню
+      const link = item.querySelector('a');
+      // и добавить на неё обработчик клика, который скроет меню
+      link.addEventListener('click', toggleMenu);
+    });
+  }
+
+  // Функция открытия/закрытия главного меню
+  function toggleMenu() {
+    Object.keys(elements).forEach(element => {
+      if (elements[element] !== undefined) {
+        elements[element].classList.toggle(`${element}--opened`);
+        elements[element].classList.toggle(`${element}--closed`);
+      }
+    });
+
+    if (mainElement) {
+      mainElement.classList.toggle('main--menu-open');
+    }
+  }
+
+})();
